f944251ce6efc40cd6e403fe115c659b7bf3d872
hank amadeus <hankamadeus@email.com>
2020-06-12 21:58:42 -0400

    established html routes for landing and notes page, can now display on server

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 
+// allow users to create and save notes (POST)
 
+// allow users to view previous saved notes (GET)
+
+// allow users to delete previous saved notes
 
 
 module.exports = router;
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,5 +1,19 @@
 const router = require("express").Router();
+const path = require('path');
 
+// HTML ROUTES
+
+// Route to landing page
+router.get("/", (req, res) => {
+    const filePath = path.join(__dirname, "..", "public", "index.html");
+    res.sendFile(filePath);
+});
+
+// Route to notes page
+router.get("/notes", (req, res) => {
+    const filePath = path.join(__dirname, "..", "public", "notes.html");
+    res.sendFile(filePath);
+});
 
 
 
