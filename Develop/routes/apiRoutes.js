c57a681a6b1d4e6c65d84e70e9b8be401c3277d6
hank amadeus <hankamadeus@email.com>
2020-06-12 22:41:23 -0400

    added middleware to server and basic post function to sav notes...WIP

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 
-// allow users to create and save notes (POST)
+// allow users to create and save notes 
+router.post("/notes", (req, res) => {
+    const { title, text } = req.body;
+    console.log(title, text);
+});
 
-// allow users to view previous saved notes (GET)
+// allow users to view previous saved notes 
+
 
 // allow users to delete previous saved notes
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const PORT = 8080;
 
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+
+// Middleware that is needed to use POST data 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Middleware to load css file
+// CITED: https://stackoverflow.com/questions/34174133/why-does-omitting-the-line-app-useexpress-static-dirname-public-stop-my
+app.use(express.static(path.join(__dirname, "public")));
+
+
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
