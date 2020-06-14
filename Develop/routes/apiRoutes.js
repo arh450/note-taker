ad40f50462aa03e6cb3732bc4ddb74efd6d955b7
hank amadeus <hankamadeus@email.com>
2020-06-13 23:24:08 -0400

    added post function to successfully post new notes to db and then return back to html

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -12,15 +12,30 @@ const dbNotes = JSON.parse(fs.readFileSync(db, (err, data) => {
 })
 );
 
+const updateDb = (dbNotes) => {
+    fs.writeFileSync(db, JSON.stringify(dbNotes), (err) => {
+        if (err) {
+            throw (err);
+        }
+    });
+}
 
 // get request to return data in db to html
 router.get("/notes", (req, res) => {
     return res.json(dbNotes);
-    console.log("Data from db has been returned");
 });
-// allow users to create and save notes 
+// Post request that saves new note from client and updates db 
 router.post("/notes", (req, res) => {
-
+    // variable to request post from client (a new note)
+    let newNote = req.body;
+    // adds id using uuid to post from client (a new note)
+    newNote.id = uuidv4();
+    // adds post from client (a new note) to the db
+    dbNotes.push(newNote);
+    // function to synchronously write file for db
+    updateDb(dbNotes);
+    // return new data from db to html file 
+    return res.json(dbNotes);
 });
 
 // allow users to delete previous saved notes
