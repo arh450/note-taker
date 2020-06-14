b78ebb69f367d4d2887b92b67eb8b5ecc30da625
hank amadeus <hankamadeus@email.com>
2020-06-14 14:57:32 -0400

    refactored get/post functions to avoid scope issues, and was able to then write delete function

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,44 +4,71 @@ const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 const db = path.join(__dirname, "../db/db.json");
 
-// Variable that parses data in db and using readFile will be then used to return notes in db to html
-const dbNotes = JSON.parse(fs.readFileSync(db, (err, data) => {
-    if (err) {
-        throw (err);
-    }
-})
-);
-
-const updateDb = (dbNotes) => {
-    fs.writeFileSync(db, JSON.stringify(dbNotes), (err) => {
-        if (err) {
-            throw (err);
-        }
-    });
-}
-
-// get request to return data in db to html
+// get request to return data in db file to client on html
 router.get("/notes", (req, res) => {
-    return res.json(dbNotes);
+    // read data from db using readFile
+    fs.readFile(db, "utf8", (err, data) => {
+        res.json(JSON.parse(data));
+        console.log("Notes Returned");
+    });
 });
+
 // Post request that saves new note from client and updates db 
 router.post("/notes", (req, res) => {
-    // variable to request post from client (a new note)
+    // variable for new note to save on the request body
     let newNote = req.body;
-    // adds id using uuid to post from client (a new note)
+    // assign id to the new note using .uuidv4() 
     newNote.id = uuidv4();
-    // adds post from client (a new note) to the db
-    dbNotes.push(newNote);
-    // function to synchronously write file for db
-    updateDb(dbNotes);
-    // return new data from db to html file 
-    return res.json(dbNotes);
+
+    // records new note to db file and then returns new note back to client on html
+    fs.readFile(db, "utf8", (err, data) => {
+        let note = JSON.parse(data);
+        note.push(newNote);
+
+        fs.writeFile(db, JSON.stringify(note), (err) => {
+            if (err) {
+                throw (err);
+
+            }
+            console.log("Note Recorded");
+        });
+    })
+    res.json(newNote);
+
+
 });
 
 // allow users to delete previous saved notes
 router.delete("/notes/:id", (req, res) => {
 
+    fs.readFile(db, "utf8", (err, data) => {
+        // paramter that contains the id of a note that is going to be deleted
+        let noteId = req.params.id;
+        let noteData = JSON.parse(data);
+        // filter noteData using .filter() to ensure the selected note is deleted
+        noteData = noteData.filter((note) => {
+            if (noteId != note.id) {
+                return true;
+            } else {
+                return false;
+            };
+        });
+        // then rewrite notes to db file
+        fs.writeFile(db, JSON.stringify(noteData), function (err) {
+            if (err)
+                throw err;
+            res.send(console.log("Note Deleted"));
+        })
+    });
+
+
+
 });
+
+
+
+
+
 
 
 module.exports = router;
