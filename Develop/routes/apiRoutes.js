61f23b4c59b5cf2625f3813726c532e2b3168830
hank amadeus <hankamadeus@email.com>
2020-06-13 22:36:12 -0400

    added get request to reuturn db note data to notes html

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,20 +1,26 @@
 const router = require("express").Router();
 const fs = require("fs");
-const { uuid } = require('uuidv4');
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const db = path.join(__dirname, "../db/db.json");
+
+// Variable that parses data in db and using readFile will be then used to return notes in db to html
+const dbNotes = JSON.parse(fs.readFileSync(db, (err, data) => {
+    if (err) {
+        throw (err);
+    }
+})
+);
 
 
-
-
-
-// allow users to view previous saved notes 
+// get request to return data in db to html
 router.get("/notes", (req, res) => {
-    console.log("Test Get");
+    return res.json(dbNotes);
+    console.log("Data from db has been returned");
 });
 // allow users to create and save notes 
 router.post("/notes", (req, res) => {
 
-
-    console.log("Test post");
 });
 
 // allow users to delete previous saved notes
