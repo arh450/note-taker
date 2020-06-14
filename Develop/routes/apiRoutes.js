5101e001684dba529a3bf0b0a2ddb5b7a8aebd3f
hank amadeus <hankamadeus@email.com>
2020-06-13 22:01:25 -0400

    moving away from notebook class and going to use functions in apiroutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,25 @@
 const router = require("express").Router();
 const fs = require("fs");
-const notes = require("../db/db.json");
+const { uuid } = require('uuidv4');
 
 
 
+
+
+// allow users to view previous saved notes 
+router.get("/notes", (req, res) => {
+    console.log("Test Get");
+});
 // allow users to create and save notes 
 router.post("/notes", (req, res) => {
 
+
+    console.log("Test post");
 });
 
-// allow users to view previous saved notes 
-
-
 // allow users to delete previous saved notes
-
+router.delete("/notes", (req, res) => {
+    console.log("Test delete");
+});
 
 module.exports = router;
